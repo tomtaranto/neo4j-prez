d1a023049894587b724fcf46f2b7e632c72755ca
tom <ttaranto@ekinox.io>
2023-04-12 23:37:21 +0200

    removing commentaries

--- main.py
+++ main.py
@@ -9,9 +9,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-# Pour run:
-# sudo docker run     --publish=7474:7474 --publish=7687:7687     --volume=$HOME/neo4j/data:/data     neo4j
-# Disponible : http://localhost:7474/
-
-# docker run --name postgre-trip   -e POSTGRES_PASSWORD=postgres   -e POSTGRES_DB=postgres   -e POSTGRES_USER=postgres   -v "/home/tom/tmp-postgre:/var/lib/postgresql/data"   -d -p 5432:5432 postgres
